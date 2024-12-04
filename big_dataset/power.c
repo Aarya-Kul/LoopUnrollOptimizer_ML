9c5da0eea118bb5d64fef9043682e44386ac6fe7
amszuch <amszuch@umich.edu>
2024-12-04 02:21:57 -0500

    big dataset

---
+++ big_dataset/power.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int power(int base, int exp) {
+    int result = 1;
+    for (int i = 0; i < exp; i++) {
+        result *= base;
+    }
+    return result;
+}
+
+int main() {
+    int base = 2, exp = 8;
+    int result = power(base, exp);
+    return 0;
+}
